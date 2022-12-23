b2bb82136da88a0a8889bf2462d45f4f15ace3d3
Yuta Akiya <yutaakiya51@gmail.com>
2022-12-23 13:38:50 -0800

    copy over config from prev repo

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -21,6 +21,8 @@ export const serverSchema = z.object({
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  BATTLENET_CLIENT_ID: z.string(),
+  BATTLENET_CLIENT_SECRET: z.string(),
 });
 
 /**
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1280px',
+      },
+    },
+    extend: {
+      screens:{
+        mb: '768px',
+      }
+    }
   },
   plugins: [],
 };
